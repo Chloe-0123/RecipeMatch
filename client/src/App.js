94eb8b5fd4988f9313c9df7b5b67742dd6baa8a9
Chloe-990123 <118889541+Chloe-990123@users.noreply.github.com>
2023-08-29 16:14:32 +0900

    client

---
+++ client/src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+import { useState, useEffect } from 'react';
+import axios from 'axios'
+
+function App() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get('/api/recipes', {
+          params: {
+            ingredients: 'apple,++flour,+sugar&number=2', // Replace with user's input
+          },
+        });
+        setRecipes(response.data);
+       
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  console.log(recipes)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{recipes.length === 0 ?  "Loading..." : recipes[0].title}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
