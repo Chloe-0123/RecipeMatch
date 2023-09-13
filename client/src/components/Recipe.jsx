78209bf4c445da93ca997df3d89af605b1a4c21c
Chloe-990123 <118889541+Chloe-990123@users.noreply.github.com>
2023-09-13 21:17:08 +0900

    recipes css

--- client/src/components/Recipe.jsx
+++ client/src/components/Recipe.jsx
@@ -7,13 +7,13 @@ export const Recipe = ({ recipe }) => {
   return (
     <>
     <ThemeProvider theme={colortheme}>
-        <div className="container tw-flex tw-p-[1rem] tw-justify-between tw-border-b-[1px] tw-border-slate-500 tw-border-dashed">
-            <div className="m tw-flex tw-flex-col">
-                <h3 className='tw-text-[1.2rem]'>{recipe.title}</h3>
-                <p className='tw-text-[0.8rem] tw-mb-[1rem]'><strong>Missing Ingredients: </strong>{recipe.missedIngredients.length !== 0 ? ''+recipe.missedIngredients.map((ing) => ing.name): 'None'}</p>
-                <Button variant="contained" sx={{width: '100px', height: '20px', fontSize: '10px', boxShadow: 'none', borderRadius: '8px'}} color='orange'>Save</Button>
+        <div className="container tw-flex tw-p-[1rem] tw-justify-between tw-border-b-[1px] tw-border-slate-500 tw-border-dashed md:tw-justify-center">
+            <div className="m tw-flex tw-flex-col md:tw-w-[50%]">
+                <h3 className='tw-text-[1.2rem] md:tw-text-[1.6rem]'>{recipe.title}</h3>
+                <p className='tw-text-[0.8rem] tw-mb-[1rem] md:tw-text-[1rem]'><strong>Missing Ingredients: </strong>{recipe.missedIngredients.length !== 0 ? ''+recipe.missedIngredients.map((ing) => ing.name): 'None'}</p>
+                <Button sx={{boxShadow: 'none'}} variant="contained" color='orange' className='tw-w-[100px] tw-h-[20px] tw-text-[10px] tw-rounded-[8px] md:tw-h-[30px]'>Save</Button>
             </div>
-            <img src={recipe.image} alt="" className='tw-w-[100px] tw-h-[100px]'/>
+            <img src={recipe.image} alt="" className='tw-w-[100px] tw-h-[100px] md:tw-w-[150px] md:tw-h-[150px]'/>
         </div>
 
     
