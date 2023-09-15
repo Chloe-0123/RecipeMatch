77752e4fba51f9283a9bfda2021a3fae572925f3
Chloe-990123 <118889541+Chloe-990123@users.noreply.github.com>
2023-09-15 16:32:27 +0900

    edits to model

--- server/models/Recipe.js
+++ server/models/Recipe.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const RecipeSchema = new mongoose.Schema({
     userEmail: { type: String, unique: false },
-    recipeId: { type: String, unique: true },
-    recipeTitle: { type: String, unique: true },
-    recipeImage: { type: String, unique: true },
+    recipeId: { type: String, unique: false },
+    recipeTitle: { type: String, unique: false },
+    recipeImage: { type: String, unique: false },
   });
 
 module.exports = mongoose.model("Recipe", RecipeSchema);
